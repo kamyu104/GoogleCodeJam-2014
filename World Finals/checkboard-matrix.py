6163ee9422ea524fd448f165c7ce1f19516a8510
Allen Liu <kamyu104@gmail.com>
2015-07-13 22:45:56 +0800

    add solution

---
+++ World Finals/checkboard-matrix.py
@@ -0,0 +1,61 @@
+def count_swaps(pos):
+    nswaps = 0
+    for i in pos:
+        if i % 2 == 0:
+            nswaps = nswaps + 1
+    return nswaps
+
+
+def inverse(A):
+    return [chr(ord('0') + ord('1') - ord(c)) for c in A]
+
+# min_swaps returns minimum swaps required to 
+# form alternating rows. If B is an invalid matrix, 
+# returns -1 to denote an impossible case.
+def min_swaps(M, N):
+    # Step 1.
+    typeA = M[0]
+    typeB = inverse(typeA)
+
+    pos_A = []
+    pos_B = []
+    for i in xrange(2 * N):
+        # Step 2 a.
+        if M[i] == typeA:
+            pos_A.append(i)
+        # Step 2 b.
+        elif M[i] == typeB:
+            pos_B.append(i)
+        # Step 2 c.
+        else:
+            return -1
+    # Step 3.
+    if len(pos_A) != len(pos_B):
+        return -1
+
+    # Step 4.
+    return min(count_swaps(pos_A), count_swaps(pos_B))
+
+
+def checkboard_matrix(M, N):
+    # Step 1-4.
+    row_swaps = min_swaps(M, N)
+    if row_swaps == -1:
+        return "IMPOSSIBLE"
+
+    Mt = [list(i) for i in zip(*M)]    # Transpose matrix M
+
+    # Step 5.
+    col_swaps = min_swaps(Mt, N)
+    if col_swaps == -1:
+        return "IMPOSSIBLE"
+
+    return row_swaps + col_swaps
+
+
+for case in xrange(input()):
+    N = input()
+    M = []
+    for i in xrange(2 * N):
+        M.append(list(raw_input()))
+    print "Case #%d: %s" % (case+1, checkboard_matrix(M, N))
