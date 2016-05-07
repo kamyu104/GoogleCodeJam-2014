c1cd6ad8dcb4e3da29a9f9c16eb285605ccf4eba
Allen Liu <kamyu@AllendeMacBook-Pro.local>
2016-05-08 01:32:18 +0800

    update

--- Round 1C/reordring-train-cars.py
+++ Round 1C/reordring-train-cars.py
@@ -3,7 +3,7 @@
 # Google Code Jam 2016 Round 1C - Problem B. Reordering Train Cars
 # https://code.google.com/codejam/contest/3004486/dashboard#s=p1
 #
-# Time:  O(N)
+# Time:  O(L), L is the set of connected Cars' length.
 # Space: O(1)
 #
 
@@ -21,56 +21,77 @@ def reordring_train_cars():
     N = int(input())
     cars = map(unique, raw_input().strip().split())
     
-    single_chars, begin, end = defaultdict(int), {}, {}
-    for s in cars:
+    begin, end, middle, single_chars = {}, {}, {}, defaultdict(int)
+    for i, s in enumerate(cars):
         if len(s) == 1:
             single_chars[s[0]] += 1
         else:
+            # There is no letter repeated in
+            # two different places in the string.
+            if s[0] == s[-1]:
+                return 0
+
+            # It appears at most once in the
+            # beginning of one string.
             if s[0] in begin:
                 return 0
             else:
-                begin[s[0]] = s
+                begin[s[0]] = i
 
+            # It appears at most once at the
+            # end of one string.
             if s[-1] in end:
                 return 0
             else:
-                end[s[-1]] = s
+                end[s[-1]] = i
 
-    for s in begin.values():
-        for i in xrange(1, len(s) - 1):
-            if s[i] in begin or s[i] in end:
-                return 0
+            # It appears in the middle of one string,
+            # and nowhere else.
+            for j in xrange(1, len(s) - 1):
+                if any([s[j] in begin, \
+                        s[j] in end, \
+                        s[j] in middle]):
+                    return 0
+                else:
+                    middle[s[j]] = i
 
-    ways = 1
+    # It appears any number of times as
+    # a single-character string.
+    num_groups, ways = 0, 1
     for c in single_chars.keys():
         ways = (ways * factorials[single_chars[c]]) % MOD
         if c not in begin and c not in end:
-            begin[c] = end[c] = c
+            num_groups += 1
 
-    n = 0
     while len(begin) > 0:
-        _, s = begin.popitem()
+        # Form each disjoint group.
+        _, i = begin.popitem()
+        s = cars[i]
         del end[s[-1]]
 
         while s[-1] in begin:
-            s += begin.pop(s[-1])
+            s += cars[begin.pop(s[-1])]
             del end[s[-1]]
         
         while s[0] in end:
-            s = end.pop(s[0]) + s
+            s = cars[end.pop(s[0])] + s
             del begin[s[0]]
 
+        # All occurrences of the same character
+        # should be adjacent to each other.
         if len(s) > 1 and s[0] == s[-1]:
             return 0
 
-        n += 1
+        num_groups += 1
     
-    ways = (ways * factorials[n]) % MOD
+    ways = (ways * factorials[num_groups]) % MOD
     
     return ways
 
-# Precompute factorials.
+
 MOD = int(1e9 + 7)
+
+# Precompute factorials.
 factorials = [1]
 for i in xrange(1, 105):
     factorials.append((factorials[-1] * i) % MOD)
