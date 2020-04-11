607480065c36277a09978eace104d1f4d1d074a1
Allen Liu <kamyu104@gmail.com>
2020-04-12 04:07:05 +0800

    update

--- Round 1A/proper_shuffle.py
+++ Round 1A/proper_shuffle.py
@@ -3,7 +3,7 @@
 # Google Code Jam 2014 Round 1A - Problem C. Proper Shuffle
 # https://code.google.com/codejam/contest/2984486/dashboard#s=p2
 #
-# Time:  precompute: O(N^3)
+# Time:  precompute: O(N^2)
 #        classify:   O(N) 
 # Space: O(N^2)
 #
@@ -13,28 +13,24 @@ from math import log
 
 def proper_shuffle():
     N = input()
-    P = map(int, raw_input().strip().split())
+    S = map(int, raw_input().strip().split())
     log_good_prob, log_bad_prob = -N*log(N), 0.0
     for i in xrange(N):
-        log_bad_prob += log(F[i][P[i]])
+        log_bad_prob += log(f[S[i]][i])
     return "GOOD" if log_good_prob > log_bad_prob else "BAD"
 
-'''
 MAX_N = 1000
 P_MOVE = 1.0/MAX_N
 P_STAY = 1.0-P_MOVE
-F = [[float(i == j) for j in xrange(MAX_N)] for i in xrange(MAX_N)]
+f = [[float(i == j) for j in xrange(MAX_N)] for i in xrange(MAX_N)]
+g, h = [0.0]*MAX_N, [1.0]*MAX_N
+for k in xrange(MAX_N):
+    for i in xrange(MAX_N):
+        g[i] += h[i]*f[i][k]*P_MOVE
+        h[i] *= P_STAY
+        f[i][k] = (P_MOVE-g[i]) / h[i]
 for i in xrange(MAX_N):
-    print >>stderr, i
     for j in xrange(MAX_N):
-        if j == i:
-            continue
-        for k in xrange(MAX_N):
-            F[j][k] = F[j][k]*P_STAY + F[i][k]*P_MOVE
-    for k in xrange(MAX_N):
-        F[i][k] = P_MOVE
-print >>open("proper_shuffle_precompute.txt", "w"), F
-'''
-F = eval(open("proper_shuffle_precompute.txt", "r").read())
+        f[i][j] = h[i]*f[i][j] + g[i]
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, proper_shuffle())
